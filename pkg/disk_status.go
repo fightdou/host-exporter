c6fdc11092dce0d58081f6105d748e9e99034eae
douyali <douyali@troila.com>
2022-03-24 18:08:40 +0800

    edit

--- pkg/disk_status.go
+++ pkg/disk_status.go
@@ -1,12 +1,9 @@
 package pkg
 
 import (
-	"bufio"
-	"io"
-	"io/ioutil"
-	"os"
-	"regexp"
-	"strings"
+	"encoding/json"
+	"fmt"
+	"strconv"
 
 	"github.com/go-kit/log"
 	"github.com/go-kit/log/level"
@@ -14,28 +11,58 @@ import (
 )
 
 type DiskStatusCollector struct {
-	hostDiskStatus *prometheus.Desc
+	hostPhysicalDiskStatus *prometheus.Desc
+	hostVirtualDiskStatus *prometheus.Desc
 	hostRaidStatus *prometheus.Desc
 	logger         log.Logger
 }
 
-type deviceInfo struct {
-	slotNumber string
-	state      string
+type Response struct {
+	Controllers []struct {
+		CommandStatus struct {
+			Controller int      `json:"Controller"`
+			Status     string   `json:"Status"`
+		} `json:"Command Status"`
+		ResponseData struct {
+			VirtualDrives int `json:"Virtual Drives"`
+			VDList        []struct {
+				Position string `json:"DG/VD"`
+				Type     string `json:"TYPE"`
+				State    string `json:"State"`
+				Size     string `json:"Size"`
+			} `json:"VD LIST"`
+			PhysicalDrives int `json:"Physical Drives"`
+			PDList         []struct {
+				Device   int    `json:"DID"`
+				Position string `json:"EID:Slt"`
+				State    string `json:"State"`
+				Media    string `json:"Med"`
+				Model    string `json:"Model"`
+				Size     string `json:"Size"`
+				Type     string `json:"Type"`
+			} `json:"PD LIST"`
+		} `json:"Response Data"`
+	} `json:"Controllers"`
 }
 
 func NewDiskStatusCollector(promLog log.Logger) *DiskStatusCollector {
 	return &DiskStatusCollector{
-		hostDiskStatus: prometheus.NewDesc(
-			"host_disk_status",
+		hostPhysicalDiskStatus: prometheus.NewDesc(
+			"host_physical_disk_status",
 			"The host disk status check (state=UBUnsp) instructions disk abnormal",
-			[]string{"slotNumber", "state"},
+			[]string{"controller", "slot", "device", "model", "state", "media", "size"},
+			nil,
+		),
+		hostVirtualDiskStatus: prometheus.NewDesc(
+			"host_virtual_disk_status",
+			"The host disk status check (state=UBUnsp) instructions disk abnormal",
+			[]string{"controller", "slot", "type", "size", "state"},
 			nil,
 		),
 		hostRaidStatus: prometheus.NewDesc(
-			"host_raid_status",
+			"host_raid_card_status",
 			"The host raid status check(0=abnormal, 1=normal)",
-			[]string{"raidCardName"},
+			[]string{"controller"},
 			nil,
 		),
 		logger: promLog,
@@ -43,141 +70,63 @@ func NewDiskStatusCollector(promLog log.Logger) *DiskStatusCollector {
 }
 
 func (d *DiskStatusCollector) Describe(ch chan<- *prometheus.Desc) {
-	ch <- d.hostDiskStatus
+	ch <- d.hostPhysicalDiskStatus
+	ch <- d.hostVirtualDiskStatus
 	ch <- d.hostRaidStatus
 }
 
 func (d *DiskStatusCollector) Collect(ch chan<- prometheus.Metric) {
-	raidName, raidStatus, diskInfo := d.getDiskStatusInfo()
-	level.Debug(d.logger).Log("msg", "Get raid card name", raidName, "Get raid current status", raidStatus)
+	response, err := d.getDiskStatusInfo()
+	if err != nil {
+		level.Error(d.logger).Log("msg", "Failed to fetch StorCLI output")
+	}
+
 	var value float64
-	for _, va := range diskInfo {
-		if va.state == "UBUnsp" {
+	value = 1
+	for _, controller := range response.Controllers {
+		for _, virtualDrive := range controller.ResponseData.VDList {
+			ch <- prometheus.MustNewConstMetric(
+				d.hostVirtualDiskStatus, prometheus.GaugeValue, value,
+				strconv.Itoa(controller.CommandStatus.Controller), virtualDrive.Position, virtualDrive.Type, virtualDrive.Size, virtualDrive.State,
+			)
+		}
+		for _, physicalDrive := range controller.ResponseData.PDList {
+			if physicalDrive.State == "UBUnsp" {
+				value = 0
+			}
+			ch <- prometheus.MustNewConstMetric(
+				d.hostPhysicalDiskStatus, prometheus.GaugeValue, value,
+				strconv.Itoa(controller.CommandStatus.Controller), physicalDrive.Position, strconv.Itoa(physicalDrive.Device), physicalDrive.Model,
+				physicalDrive.State, physicalDrive.Media, physicalDrive.Size,
+			)
+		}
+		if controller.CommandStatus.Status != "Success" {
 			value = 0
-		} else {
-			value = 1
 		}
 		ch <- prometheus.MustNewConstMetric(
-			d.hostDiskStatus,
+			d.hostRaidStatus,
 			prometheus.GaugeValue,
 			value,
-			va.slotNumber,
-			va.state,
+			strconv.Itoa(controller.CommandStatus.Controller),
 		)
 	}
-	ch <- prometheus.MustNewConstMetric(
-		d.hostRaidStatus,
-		prometheus.GaugeValue,
-		raidStatus,
-		raidName,
-	)
 	level.Info(d.logger).Log("msg", "collectd disk status success")
 }
 
-func (d *DiskStatusCollector) getDiskStatusInfo() (string, float64, map[string]deviceInfo) {
+func (d *DiskStatusCollector) getDiskStatusInfo() (resp Response, err error) {
 	args := []string{
-		"/c0",
+		"/call",
 		"show",
+		"all",
+		"J",
 		"nolog",
 	}
 	results, err := Execute("storcli64", args...)
-	level.Debug(d.logger).Log("msg", "Exec command storcli64", "command args", args)
+	var response Response
+	err = json.Unmarshal(results, &response)
 	if err != nil {
+		return Response{}, fmt.Errorf("Failed to unmarshal json %s ", err)
 		level.Error(d.logger).Log("msg", "Exec command storcli64 failed")
 	}
-	raidName, raidStatus, diskInfo := parseDiskInfo(results)
-	return raidName, raidStatus, diskInfo
-}
-
-func parseDiskInfo(results []byte) (string, float64, map[string]deviceInfo) {
-	var diskInfo map[string]deviceInfo
-	var devices []string
-	record := 1
-	parse := false
-
-	var raidStatus float64
-	var raidName string
-
-	file, err := ioutil.TempFile("/tmp/", "*")
-	if err != nil {
-		return "", 0, nil
-	}
-	defer os.Remove(file.Name())
-	if _, err = file.Write(results); err != nil {
-		return "", 0, nil
-	}
-	f, err := os.Open(file.Name())
-	if err != nil {
-		return "", 0, nil
-	}
-	br := bufio.NewReader(f)
-	for {
-		line, err := br.ReadString('\n')
-		if err != nil || io.EOF == err {
-			break
-		}
-		if strings.ContainsAny(line, "=") {
-			element := strings.Split(line, "=")
-			if strings.HasPrefix(element[0], "Status") {
-				status := strings.TrimSpace(element[1])
-				if status == "Success" {
-					raidStatus = 1
-				} else {
-					raidStatus = 0
-				}
-			}
-			if strings.HasPrefix(element[0], "Product Name") {
-				element := strings.Split(line, "=")
-				name := strings.TrimSpace(element[1])
-				raidName = strings.TrimSpace(name)
-			}
-		}
-		if strings.HasPrefix(line, "PD LIST") {
-			parse = true
-			record = 1
-			devices = []string{}
-		}
-		if strings.HasPrefix(line, "------------") {
-			record += 1
-		} else if record == 3 {
-			devices = append(devices, line)
-		} else if record == 4 {
-			if parse {
-				diskInfo = parseDevice(devices)
-			}
-			parse = false
-		}
-	}
-	return raidName, raidStatus, diskInfo
-}
-
-func parseDevice(devices []string) map[string]deviceInfo {
-	deviceResult := make(map[string]deviceInfo)
-	deInfo := deviceInfo{}
-	for _, device := range devices {
-		device = deleteExtraSpace(device)
-		element := strings.Split(device, " ")
-		if len(element) < 13 {
-			continue
-		}
-		slotNumber := strings.TrimSpace(strings.Split(element[0], ":")[1])
-		deInfo.slotNumber = slotNumber
-		deInfo.state = strings.TrimSpace(element[2])
-		deviceResult[slotNumber] = deInfo
-	}
-	return deviceResult
-}
-
-func deleteExtraSpace(s string) string {
-	s1 := strings.Replace(s, "  ", " ", -1)
-	regStr := "\\s{2,}"
-	reg, _ := regexp.Compile(regStr)
-	s2 := make([]byte, len(s1))
-	copy(s2, s1)
-	spcIndex := reg.FindStringIndex(string(s2))
-	for len(spcIndex) > 0 {
-		s2 = append(s2[:spcIndex[0]+1], s2[spcIndex[1]:]...)
-		spcIndex = reg.FindStringIndex(string(s2))
-	}
-	return string(s2)
+	return response, nil
 }
