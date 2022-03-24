2e6ba7be982fa6873969d7d53a921cef3c0a4d97
douyali <douyali@troila.com>
2022-03-24 08:57:13 +0800

    edit

--- pkg/cpu_temp.go
+++ pkg/cpu_temp.go
@@ -4,12 +4,13 @@ import (
 	"bytes"
 	"encoding/csv"
 	"fmt"
-	"github.com/go-kit/log"
-	"github.com/go-kit/log/level"
-	"github.com/prometheus/client_golang/prometheus"
 	"math"
 	"strconv"
 	"strings"
+
+	"github.com/go-kit/log"
+	"github.com/go-kit/log/level"
+	"github.com/prometheus/client_golang/prometheus"
 )
 
 type CpuCollector struct {
